9b2be310e742ad5a7615a4f566f90a35baaa8257
kocurvik <kocurvik@gmail.com>
2021-02-11 15:02:46 +0100

    channels + mobilenet options + multiprocessing in fit

--- train.py
+++ train.py
@@ -16,7 +16,8 @@ def parse_command_line():
     parser.add_argument('-o', '--heatmap_size', type=int, default=64, help='size of output heatmaps')
     parser.add_argument('-e', '--epochs', type=int, default=50, help='max number of epochs')
     parser.add_argument('-g', '--gpu', type=int, default=0, help='which gpu to use')
-    parser.add_argument('-bn', '--batch_norm', action='store_true', default=False)
+    parser.add_argument('-m', '--mobilenet', action='store_true', default=False)
+    parser.add_argument('-c', '--channels', type=int, default=256, help='number of channels in network')
     parser.add_argument('-exp', '--experiment', type=int, default=0, help='experiment number')
     # parser.add_argument('-s', '--steps', type=int, default=10000, help='steps per epoch')
     parser.add_argument('path')
@@ -35,8 +36,19 @@ def train():
     print("Heatmap size: {} x {}".format(args.heatmap_size, args.heatmap_size))
     print("Training for {} epochs".format(args.epochs))
     print("Scales: ", scales)
+    print("Channels: ", args.channels)
+    print("Experiment number: ", args.experiment)
+    print("Mobilenet version: ", args.mobilenet)
 
-    snapshot_dir_name = 'VP1VP2_{}in_{}out_{}s_{}n_{}b_{}'.format(args.input_size, args.heatmap_size, len(scales), args.num_stacks, args.batch_size, args.experiment)
+    if args.mobilenet:
+        module = 'mobilenet'
+        module_str = 'm'
+    else:
+        module = 'bottleneck'
+        module_str = 'b'
+
+
+    snapshot_dir_name = 'VP1VP2{}_{}in_{}out_{}s_{}n_{}b_{}c_{}'.format(module_str, args.input_size, args.heatmap_size, len(scales), args.num_stacks, args.batch_size, args.channels, args.experiment)
     snapshot_dir_path = os.path.join('snapshots', snapshot_dir_name)
 
     if not os.path.exists(snapshot_dir_path):
@@ -44,7 +56,7 @@ def train():
 
     print("Checkpoint dir name: ", snapshot_dir_name)
 
-    model = create_hourglass_network(2 * len(scales), args.num_stacks, inres=args.input_size, outres=args.heatmap_size, num_channels=256)
+    model = create_hourglass_network(2 * len(scales), args.num_stacks, inres=args.input_size, outres=args.heatmap_size, bottleneck=module, num_channels=args.channels)
 
     if args.resume:
         model.load_weights(os.path.join(snapshot_dir_path, 'model.{:03d}.h5'.format(args.resume)))
@@ -57,9 +69,9 @@ def train():
     val_dataset = BoxCarsDataset(args.path, 'val', batch_size=args.batch_size, img_size=args.input_size, heatmap_size=args.heatmap_size, scales=scales)
 
     callbacks = [keras.callbacks.ModelCheckpoint(filepath=os.path.join(snapshot_dir_path, 'model.{epoch:03d}.h5')),
-                 keras.callbacks.TensorBoard(log_dir=os.path.join('logs' ,snapshot_dir_name))]
+                 keras.callbacks.TensorBoard(log_dir=os.path.join('logs', snapshot_dir_name))]
 
-    model.fit_generator(train_dataset, validation_data=val_dataset, epochs=args.epochs, callbacks=callbacks, initial_epoch=args.resume)
+    model.fit_generator(train_dataset, validation_data=val_dataset, epochs=args.epochs, callbacks=callbacks, initial_epoch=args.resume, workers=2, use_multiprocessing=True)
 
 
 if __name__ == '__main__':
