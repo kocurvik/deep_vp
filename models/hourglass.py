94a4e7381153540058ceff77b679fcb99785998c
kocurvik <kocurvik@gmail.com>
2021-02-18 02:14:36 +0100

    refactor model loading

--- models/hourglass.py
+++ models/hourglass.py
@@ -1,3 +1,5 @@
+import os
+
 from tensorflow import keras
 import tensorflow as tf
 
@@ -211,3 +213,43 @@ def create_heads(prelayerfeatures, rf1, num_classes, hgid, num_channels):
 
     head_next_stage = keras.layers.Add()([head, head_m, prelayerfeatures])
     return head_next_stage, head_parts
+
+
+def load_model(args, scales):
+    print("Initializing model")
+    print("Batch size: ", args.batch_size)
+    print("Num stacks: ", args.num_stacks)
+    print("Input size: {} x {}".format(args.input_size, args.input_size))
+    print("Heatmap size: {} x {}".format(args.heatmap_size, args.heatmap_size))
+    print("Training for {} epochs".format(args.epochs))
+    print("Scales: ", scales)
+    print("Channels: ", args.channels)
+    print("Experiment number: ", args.experiment)
+    print("Mobilenet version: ", args.mobilenet)
+
+    if args.mobilenet:
+        module = 'mobilenet'
+        module_str = 'm'
+    else:
+        module = 'bottleneck'
+        module_str = 'b'
+
+    snapshot_dir_name = 'VP1VP2{}_{}in_{}out_{}s_{}n_{}b_{}c_{}'.format(module_str, args.input_size, args.heatmap_size,
+                                                                        len(scales), args.num_stacks, args.batch_size,
+                                                                        args.channels, args.experiment)
+    snapshot_dir_path = os.path.join('snapshots', snapshot_dir_name)
+
+    if not os.path.exists(snapshot_dir_path):
+        os.makedirs(snapshot_dir_path)
+
+    print("Checkpoint dir name: ", snapshot_dir_name)
+
+    model = create_hourglass_network(2 * len(scales), args.num_stacks, inres=args.input_size, outres=args.heatmap_size,
+                                     bottleneck=module, num_channels=args.channels)
+
+    if args.resume:
+        resume_model_path = os.path.join(snapshot_dir_path, 'model.{:03d}.h5'.format(args.resume))
+        print("Loading model", resume_model_path)
+        model.load_weights(resume_model_path)
+
+    return model, snapshot_dir_name, snapshot_dir_path
