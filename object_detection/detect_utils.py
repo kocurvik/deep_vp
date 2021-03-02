ed1bb44ef8d90f25bdab815dbefb967073619d20
kocurvik <kocurvik@gmail.com>
2021-03-02 03:28:26 +0100

    fix variable frame shapes

--- object_detection/detect_utils.py
+++ object_detection/detect_utils.py
@@ -12,10 +12,10 @@ def save(json_path, detection_list):
 
 def show_debug(frame, boxes):
     for box in boxes:
-        x_min = int(1920 * box[1])
-        y_min = int(1080 * box[0])
-        x_max = int(1920 * box[3] + 1)
-        y_max = int(1080 * box[2] + 1)
+        x_min = int(frame.shape[1] * box[1])
+        y_min = int(frame.shape[0] * box[0])
+        x_max = int(frame.shape[1] * box[3] + 1)
+        y_max = int(frame.shape[0] * box[2] + 1)
 
         frame=cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 0, 255))
 
@@ -25,12 +25,17 @@ def show_debug(frame, boxes):
 
 def show_mask_debug(frame, boxes, masks):
     for box, mask in zip(boxes, masks):
+        x_min = frame.shape[1] * box[1]
+        y_min = frame.shape[0] * box[0]
+        x_max = frame.shape[1] * box[3]
+        y_max = frame.shape[0] * box[2]
+
         rect_src = np.array([[0, 0], [mask.shape[1], 0], [mask.shape[1], mask.shape[0]], [0, mask.shape[0]]], dtype=np.float32)
-        rect_dst = np.array([[1920 * box[1], 1080 * box[0]], [1920 * box[3], 1080 * box[0]], [1920 * box[3], 1080 * box[2]], [1920 * box[1], 1080 * box[2]]], dtype=np.float32)
+        rect_dst = np.array([[x_min, y_min], [x_max, y_min], [x_max, y_max], [x_min, y_max]], dtype=np.float32)
 
         M = cv2.getPerspectiveTransform(rect_src[:, :], rect_dst[:, :])
 
-        mask_frame = cv2.warpPerspective(mask, M, (1920, 1080), flags=cv2.INTER_CUBIC)
+        mask_frame = cv2.warpPerspective(mask, M, (frame.shape[1], frame.shape[0]), flags=cv2.INTER_CUBIC)
         vis_frame = np.copy(frame)
         vis_frame[:, :, 2] = mask_frame
 
