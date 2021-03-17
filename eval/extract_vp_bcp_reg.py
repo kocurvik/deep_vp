1e7d888049a6dd93b43751ed77c7a75d41d93fe5
kocurvik <kocurvik@gmail.com>
2021-03-17 18:40:42 +0100

    save more memory in vp extract bcp mask reg version

--- eval/extract_vp_bcp_reg.py
+++ eval/extract_vp_bcp_reg.py
@@ -51,11 +51,14 @@ def detect_session(detector, model_dir_name, data_path, session, args):
 
         boxes = detection['boxes']
         scores = detection['scores']
+        del detection['boxes']
+        del detection['scores']
 
         box_cnt += len(boxes)
 
         if args.mask:
             masks = detection['masks']
+            del detection['masks']
 
         boxes, scores, masks, prev_edge = filter_boxes_bcp(boxes, scores, frame, prev_edge, masks=masks)
 
